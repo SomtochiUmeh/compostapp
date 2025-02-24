a9a0b8aa07301b8a2e495c5c0fdc434164e3ca4e
Somtochi Umeh <74836691+SomtochiUmeh@users.noreply.github.com>
2025-02-23 17:25:03 -0800

    cleanup

--- lib/services/recipe_validator_service.dart
+++
@@ -1,6 +0,0 @@
-// import '../models/recipe_model.dart';
-
-// class RecipeValidatorService {
-//   static Map<String, bool> validateRecipe(Recipe recipe) {...}
-//   static bool isRecipeComplete(Recipe recipe) {...}
-// }
--- lib/utils/price_calculator.dart
+++
@@ -1,6 +0,0 @@
-// import '../models/recipe_model.dart';
-
-// class PriceCalculator {
-//   static double calculateTotalPrice(Recipe recipe) {...}
-//   static Map<String, double> calculatePriceBreakdown(Recipe recipe) {...}
-// }
